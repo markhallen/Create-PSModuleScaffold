9b857f0e0547e4d03f5524b37231466b7573595c
Mark Allen <markhallen@gmail.com>
2017-06-29 20:44:44 +0200

    Initial commit

---
+++ Create-PSModuleScaffold.Tests.ps1
@@ -0,0 +1,77 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Create-PSModuleScaffold" {
+
+    Mock New-Item
+    Mock Copy-Item
+    $Parameters = @{
+        'Path' = 'C:\Windows\Temp'
+        'ModuleName' = 'TestModule'
+        'Author' = 'Stephen King'
+        'Description' = 'Yes, Stephen King writes code now.'
+    }
+    Mock New-ModuleManifest
+
+    Context 'accepting parameters' {
+        It 'accepts ModuleName, Author, Path and Description strings' {
+            { Create-PSModuleScaffold @Parameters } | Should Not Throw
+        }
+    }
+
+    Context 'creating directory structure' {
+        Create-PSModuleScaffold @Parameters
+        It "create the module root directory'" {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith($Parameters.ModuleName) -and $ItemType.Equals('Directory') } -Times 1 -Exactly
+        }
+        It "create a Src directory'" {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith($Parameters.ModuleName + '-Repo\' + $Parameters.ModuleName) -and $ItemType.Equals('Directory') } -Times 1 -Exactly
+        }
+        It "create a Public directory'" {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('Public') -and $ItemType.Equals('Directory') } -Times 1 -Exactly
+        }
+        It "create a Private directory'" {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('Public') -and $ItemType.Equals('Directory') } -Times 1 -Exactly
+        }
+        It "create a language directory (en-us)'" {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('en-us') -and $ItemType.Equals('Directory') } -Times 1 -Exactly
+        }
+        It "create a tests directory'" {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('Tests') -and $ItemType.Equals('Directory') } -Times 1 -Exactly
+        }
+    }
+
+    Context 'creating empty files' {
+        Create-PSModuleScaffold @Parameters
+        It 'create the .Format.ps1xml file' {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('.Format.ps1xml') -and $ItemType.Equals('File') } -Times 1 -Exactly
+        }
+        It 'create the .help.txt file' {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('.help.txt') -and $ItemType.Equals('File') } -Times 1 -Exactly
+        }
+        It 'create the Licence file' {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('Licence') -and $ItemType.Equals('File') } -Times 1 -Exactly
+        }
+        It 'create the Readme.md file' {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('README.md') -and $ItemType.Equals('File') } -Times 1 -Exactly
+        }
+        It 'create the AppVeyor.yml file' {
+            Assert-MockCalled New-Item -ParameterFilter { $Path.EndsWith('AppVeyor.yml') -and $ItemType.Equals('File') } -Times 1 -Exactly
+        }
+    }
+
+    Context 'copying template files' {
+        Create-PSModuleScaffold @Parameters
+        It 'copy the .psm1 file' {
+            Assert-MockCalled Copy-Item -ParameterFilter { $Destination.EndsWith($Parameters.ModuleName + '.psm1') } -Times 1 -Exactly
+        }
+    }
+
+    Context 'creating module maifest' {
+        Create-PSModuleScaffold @Parameters
+        It 'create the module manifest' {
+            Assert-MockCalled New-ModuleManifest -ParameterFilter { $Path.EndsWith($Parameters.ModuleName + '.psd1') } -Times 1 -Exactly
+        }
+    }
+}
---
+++ Create-PSModuleScaffold.ps1
@@ -0,0 +1,56 @@
+﻿function Create-PSModuleScaffold {
+    param(
+        # Name of the new module
+        [Parameter(Mandatory)]
+        [string]
+        $ModuleName,
+        # Author name
+        [Parameter(Mandatory)]
+        [string]
+        $Author,
+        # Author name
+        [Parameter(Mandatory)]
+        [ValidateScript({Test-Path $(Split-Path $_) -PathType 'Container'})] 
+        [string]
+        $Path,
+        # Module description
+        [Parameter(Mandatory)]
+        [string]
+        $Description
+    )
+    Begin {
+    }
+    
+    Process {
+        $Path="$Path\$ModuleName-Repo"
+        # Create the module and private function directories
+        $Directories = @($ModuleName,"$ModuleName\Private","$ModuleName\Public","$ModuleName\en-us","Tests")
+        $Directories | ForEach-Object { New-Item -Path "$Path\$_" -ItemType Directory }
+
+        #Create the module and related files
+        $Files = @(
+            "$ModuleName\$ModuleName.Format.ps1xml",
+            "$ModuleName\en-US\about_PSStackExchange.help.txt",
+            "Tests\$ModuleName.Tests.ps1",
+            "Licence",
+            ".gitignore",
+            "README.md",
+            "AppVeyor.yml"
+        )
+        $Files | ForEach-Object { New-Item -Path "$Path\$_" -ItemType File }
+
+        # Copy the base psm1 file
+        Copy-Item "$PSScriptRoot\PSModuleScaffold.psm1" "$Path\$ModuleName\$ModuleName.psm1"
+    }
+
+    End {
+        # Create the module manifest
+        New-ModuleManifest -Path "$Path\$ModuleName\$ModuleName.psd1" `
+            -RootModule "$Path\$ModuleName\$ModuleName.psm1" `
+            -Description "$Description" `
+            -PowerShellVersion 3.0 `
+            -Author "$Author" `
+            -FormatsToProcess "$ModuleName.Format.ps1xml"
+    }
+}
+#Create-PSModuleScaffold -ModuleName "CMCollectionAnalyzer" -Author "Mark Allen" -Path $pwd -Description "Analyze ConfigMgr collections for configurations that may cause site or heirachy performance issues"
